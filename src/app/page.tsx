14555d40d701523493104adef6884f2412c4b8ee
Eldrams <126687182+Eldrams@users.noreply.github.com>
2023-09-05 18:46:15 +0100

    To-do app
    refreshing the page when using server components is surprisingly difficult.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,22 @@
+
 import TodoItem from "@/components/todoItem";
 import prisma from "@/db";
 import Link from "next/link";
+import { redirect } from "next/navigation";
 
-const createServerSideProps = async () => {
-  const todos = await prisma.todo.create({data: {title: "test", complete: false }})
-  return todos
+
+async function toggleTodo(id: string, complete: boolean) {
+  "use server"
+  
+  await prisma.todo.update({where: {id}, data: {complete} })
 }
 
+async function deleteItem(id: string){
+  "use server"
+
+  await prisma.todo.delete({where: {id}})
+  redirect('/');
+}
 
 export default async function Home() {
   
@@ -17,11 +27,11 @@ export default async function Home() {
     <>
     <header className="flex justify-between items-center mb-4">
       <h1 className="text-3xl">To-Dos</h1>
-      <Link className=" text-2xl border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none" href="/new">New</Link>
+      <Link className="text-2xl border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none" href="/new">New</Link>
     </header>
     <ul className="pl-4 text-[2rem]">
       {todos.map((todo) => (
-        <TodoItem key={todo.id} {...todo}/>
+        <TodoItem deleteItem={deleteItem} toggleTodo={toggleTodo} key={todo.id} {...todo}/>
       ))}
     </ul>
     </>
--- src/components/todoItem.tsx
+++ src/components/todoItem.tsx
@@ -1,12 +1,26 @@
+"use client"
+
+import { useState } from "react";
+
 type TodoItemProps = {
     id: string;
     title: string;
     complete: boolean;
-};
-
-export default function TodoItem({id, title, complete}: TodoItemProps) {
-    return <li className="flex gap-1 items-center">
-        <input id={id} type="checkbox" className="cursor-pointer peer" />
-        <label htmlFor={id} className="cursor-pointer peer-checked:line-through peer-checked:text-slate-500">{title}</label>
-    </li>
+    toggleTodo: (id: string, complete: boolean) => void;
+    deleteItem: (id: string) => void;
 }
+  
+
+export default function TodoItem({id, title, complete, toggleTodo, deleteItem}: TodoItemProps) {
+    
+
+    return (
+        <li className="flex gap-1 items-center">
+        <input id={id} type="checkbox" className="cursor-pointer peer" defaultChecked={complete} onChange={e => toggleTodo(id, e.target.checked)}/>
+        <label htmlFor={id} className="cursor-pointer peer-checked:line-through peer-checked:text-slate-500">{title}</label>
+        <button onClick={() => deleteItem(id)} className="m-4 text-2xl border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
+          >delete item</button>
+    </li>
+    )
+}
+
